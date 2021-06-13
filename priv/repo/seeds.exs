82fa1aba5a0f0e58605f6f7ecc1bd10afe25a203
Gabriel <ggkpacker@gmail.com>
2021-06-13 15:29:16 -0300

    Use rushing.json as seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias NflRushing.Repo
+alias NflRushing.RushingStatisticTranslator
+
+require Logger
+
+rushing_statistics =
+  "priv/repo/rushing.json"
+  |> File.read!()
+  |> Jason.decode!()
+
+for rushing_statistic <- rushing_statistics do
+  Logger.info("Seeding rushing statistic #{inspect(rushing_statistic)}")
+
+  rushing_statistic
+  |> RushingStatisticTranslator.from_string_map()
+  |> Repo.insert!()
+end
